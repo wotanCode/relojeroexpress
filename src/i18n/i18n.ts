1ab7563ec822f29c971c72ea14d12600b88f38bf
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-03-16 19:19:13 -0300

    feat: Add es i18n

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,10 +1,20 @@
 import { getLocale } from "astro-i18n-aut";
 import { translations } from "./locales";
 
-export function useI18n(Astro: any) {
+type AnyRecord = Record<string, any>;
+
+export function useI18n(Astro: { url: string | URL }) {
     const locale = getLocale(Astro.url);
-    return (key: string) => {
-        const [section, term] = key.split(".");
-        return translations[locale]?.[section]?.[term] || key;
+    return (key: string): string => {
+        const keys = key.split(".");
+        let result: AnyRecord | string = translations[locale];
+
+        for (const k of keys) {
+            if (typeof result !== 'object') return key;
+            result = result[k];
+            if (result === undefined) return key;
+        }
+
+        return typeof result === 'string' ? result : key;
     };
 }
