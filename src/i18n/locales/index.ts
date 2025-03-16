549e2b16b4b595c4756cfdbc0db08873121b9fcd
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-03-16 19:19:24 -0300

    feat: add es i18n

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,9 +1,31 @@
-import esHome from '@/i18n/locales/es/home.json'
+import esCompany from '@/i18n/locales/es/company.json'
+import esContact from '@/i18n/locales/es/contact.json'
+import esFooter from '@/i18n/locales/es/footer.json'
+import esServices from '@/i18n/locales/es/services.json'
+import esHero from '@/i18n/locales/es/hero.json'
+import esMeta from '@/i18n/locales/es/meta.json'
+import esAria from '@/i18n/locales/es/aria.json'
 
-type TranslationDataT = { [key: string]: string };
+type Translations = {
+    [locale: string]: {
+        hero: typeof esHero;
+        company: typeof esCompany;
+        contact: typeof esContact;
+        footer: typeof esFooter;
+        services: typeof esServices;
+        meta: typeof esMeta;
+        aria: typeof esAria;
+    };
+};
 
-export const translations: { [locale: string]: { [section: string]: TranslationDataT } } = {
+export const translations: Translations = {
     es: {
-        home: esHome,
+        hero: esHero,
+        company: esCompany,
+        contact: esContact,
+        footer: esFooter,
+        services: esServices,
+        meta: esMeta,
+        aria: esAria
     },
 }
