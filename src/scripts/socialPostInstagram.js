21e8e46c7911091fce91dae61e691f5b38a2ec86
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2025-04-06 20:48:42 -0400

    fix: load env variable

--- src/scripts/socialPostInstagram.js
+++ src/scripts/socialPostInstagram.js
@@ -1,7 +1,10 @@
 import { promises as fs } from 'fs'
 import path from 'path'
+import dotenv from 'dotenv';
 
-const INSTAGRAM_ACCESS_TOKEN = import.meta.env.INSTAGRAM_ACCESS_TOKEN
+dotenv.config();  // load environment variables from .env file
+
+const INSTAGRAM_ACCESS_TOKEN = process.env.INSTAGRAM_ACCESS_TOKEN;
 const NUMBER_OF_POST = 6
 const PLACEHOLDER_LATEST_INSTAGRAM = '%{{latest_instagram}}%'
 
